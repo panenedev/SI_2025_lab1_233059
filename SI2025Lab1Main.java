8846b96ed448b18817183e5560b2145a5a5184e5
Pane Nedev <panenedev01@gmail.com>
2025-04-13 00:05:44 +0200

    implement countTasksPerCategory

--- SI2025Lab1Main.java
+++ SI2025Lab1Main.java
@@ -98,7 +98,7 @@ class TaskManager {
     // 7. Count tasks per category
     public Map<String, Integer> countTasksPerCategory() {
         // TODO: Implement counting logic
-        return new HashMap<>();
+        return tasks.stream().collect(Collectors.groupingBy(Task::getCategory, Collectors.reducing(0, e -> 1, Integer::sum)));
     }
 
     // 8. Mark a task as completed by name
